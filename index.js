fb01d6c548c78dfda1d80902151ca7e2437a8315
John VanSchultz <johnvanschultz@upserve.com>
2018-04-22 18:52:13 -0400

    Integrate sockets

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
+const http = require("http").Server(app);
 const port = process.env.PORT || 3000;
 const path = require("path");
+const io = require("socket.io")(http);
 
 app.set("views", __dirname + "/views");
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 app.use("/static", express.static(path.resolve(__dirname, "public")));
 app.get("/", (req, res) => res.render("index"));
+
+io.sockets.on("connection", function(socket) {
+  console.log(" a user connected");
+});
+
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "views/index.html"), function(err) {
     if (err) {
@@ -15,4 +22,5 @@ app.get("/*", function(req, res) {
     }
   });
 });
-app.listen(port, () => console.log("Example app listening on port 3000"));
+
+http.listen(port, () => console.log("App listening on port 3000"));
